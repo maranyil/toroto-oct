e7a8ae53dd272ce280de68c61ff4fcb344b0bb8d
Mara Mulato <maramulato@MacBook-Mara.local>
2021-10-26 13:25:04 -0500

    optimized styles for ProjectCard

--- src/components/projects/GridProjects.jsx
+++ src/components/projects/GridProjects.jsx
@@ -4,20 +4,47 @@ import ProjectCard from './ProjectCard';
 const GridProjects = () => {
   const [projects, setProjects] = useState();
   const [loading, setLoading] = useState(true);
+  const [err, setErr] = useState();
+
   const apiURL = 'https://fieldops-api.toroto.mx/api/projects'
+  let headers = {}
 
-  const fetchData = async () => {
-      const response = await
+  /*const fetchData = async () => {
+      const res = await
       fetch(apiURL)
-      .then((response) => response.json());
+      .then((res) => res.json());
 
-      setProjects(response.data);
+      setProjects(res.data);
       setLoading(false)
   }
 
   useEffect(() => {
       fetchData();
-  })
+  })*/
+
+  useEffect(() => {
+    fetch(apiURL, {
+      method: "GET",
+      mode: 'cors',
+      headers: Headers
+    })
+    .then(response => {
+      if (response.status === 200) {
+        return response.json()
+      }
+      throw response;
+    })
+    .then(projects => {
+      setProjects(projects.data)
+    })
+    .catch(error => {
+      console.log("oh no", error);
+      setErr(error)
+    })
+    .finally(() => {
+      setLoading(false)
+    })
+  }, [])
 
   return (
   <div className="cargando">
--- src/components/projects/ProjectCard.jsx
+++ src/components/projects/ProjectCard.jsx
@@ -1,16 +1,28 @@
 import React from 'react';
+import ClampLines from 'react-clamp-lines';
 
 const ProjectCard = ({ project, id }) => {
+  
   let imgArray = project.images;
-  console.log(imgArray);
+  let randomI = Math.floor(Math.random()*5);
+
   return (
     <div className="project-card" key={id}>
-      <img className="card-img" src={project.images[1]}/>
+      <img className="card-img" src={imgArray[randomI]}/>
       <div className="card-info">
         <h6>{project.location}</h6>
 
         <h4>{project.name}</h4>
-        <p>{project.description}</p>
+        
+        <ClampLines 
+        text={project.description}
+        id="default"
+        lines={3}
+        ellipsis="..."
+        moreText="Expand"
+        lessText="Collapse"
+        className="custom-class"
+        innerElement="p"/>
 
         <button>{project.services[0]}</button>
         <button>{project.services[1]}</button>
